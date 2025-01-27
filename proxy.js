9cc42ffe3f848625f725eeef3ea727b78bfefbed
JStreamEC <105702681+JStreamEC@users.noreply.github.com>
2025-01-27 12:16:31 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 3000; // Usar el puerto proporcionado por Replit
+const port = process.env.PORT || 10000; // Usar el puerto proporcionado por Replit
 
 app.use(express.json());
 
